62824b67dd03722b0fc58bcf8390ce4feaae3fa6
Dollar  Luo <dollarluo@Dollars-MacBook-Air.local>
2020-01-26 10:33:28 -0500

    remove bloat

--- NovaHacks/server.js
+++ NovaHacks/server.js
@@ -28,16 +28,4 @@ app.post('/print/:target', function (req, res) {
     .then(_ => octo.print(target))
 })
 
-
-
-app.get('/hello', function(req, res) {
-  const command = "/Users/dollarluo/Desktop/usdpython/stl_to_usdz.sh /Users/dollarluo/Desktop/3DMODELS/ironman.stl /Users/dollarluo/Desktop/3DMODELS/IRONMAN_MASK.usdz"
-  childProcess.exec(command, (error) => {
-    res.send('test!')
-  })
-  console.log('hi')
-})
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
