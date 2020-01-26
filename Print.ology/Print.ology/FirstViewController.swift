cb398de434dcbecfa6e2e85580b7ff2a036c5921
Dollar  Luo <dollarluo@Dollars-MacBook-Air.local>
2020-01-25 19:46:18 -0500

    initial commit

---
+++ Print.ology/Print.ology/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Print.ology
+//
+//  Created by Dollar  Luo on 2020-01-25.
+//  Copyright © 2020 Dollar  Luo. All rights reserved.
+//
+
+import UIKit/Users/dollarluo/Desktop/Print.ology/Print.ology/FirstViewController.swift
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Print.ology/Print.ology/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Print.ology
+//
+//  Created by Dollar  Luo on 2020-01-25.
+//  Copyright © 2020 Dollar  Luo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
