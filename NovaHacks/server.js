d266a62e6a5c884c34dd67bbd0a193fceea5d6d8
Dollar  Luo <dollarluo@Dollars-MacBook-Air.local>
2020-01-25 22:23:40 -0500

    add attempt

--- NovaHacks/server.js
+++ NovaHacks/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const upload = require('express-fileupload
 const childProcess = require('child_process')
 const app = express()
 const port = 3000
 
+app.use(upload())
+
 app.use('/static', express.static('/Users/dollarluo/Desktop/3DMODELS'))
+App.get('//, (req, res) => {
+	res.sendFile(__dirname + '/index.html')
 app.get('/hello', function(req, res) {
   const command = "/Users/dollarluo/Desktop/usdpython/stl_to_usdz.sh /Users/dollarluo/Desktop/3DMODELS/ironman.stl /Users/dollarluo/Desktop/3DMODELS/IRONMAN_MASK.usdz"
   childProcess.exec(command, (error) => {
