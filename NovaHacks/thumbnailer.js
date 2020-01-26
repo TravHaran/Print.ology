e393404e01e5bf43299a244a3685c2e7cf30c3c7
Dollar  Luo <dollarluo@Dollars-MacBook-Air.local>
2020-01-26 00:39:03 -0500

    add images

--- NovaHacks/thumbnailer.js
+++ NovaHacks/thumbnailer.js
@@ -2,16 +2,22 @@ const StlThumbnailer = require('node-stl-to-thumbnail')
 const path = require('path')
 const fs = require('fs')
 
-const size = { width: 250, height: 250 }
+const size = { width: 500, height: 500 }
 const modelFolder = path.resolve(__dirname, './3DMODELS')
-const stlfile = path.resolve(modelFolder, 'ironman.stl')
-const thumbnail = path.resolve(modelFolder, 'ironman.png')
 
-const thumbnailer = new StlThumbnailer({
-  filePath: stlfile,
-  requestThumbnails: [ size ]
-}).then(thumbnails => {
-  thumbnails[0].toBuffer((err, buf) => {
-    fs.writeFileSync(thumbnail, buf)
+fs.readdir(modelFolder, (err, files) => {
+  files.forEach(file => {
+    if (path.extname(file) === '.stl') {
+      const basename = path.basename(file, '.stl')
+      const thumbnailname = path.resolve(modelFolder, basename + '.png')
+      const thumbnailer = new StlThumbnailer({
+        filePath: path.resolve(modelFolder, file),
+        requestThumbnails: [ size ]
+      }).then(thumbnails => {
+        thumbnails[0].toBuffer((err, buf) => {
+          fs.writeFileSync(thumbnailname, buf)
+        })
+      })
+    }
   })
 })
