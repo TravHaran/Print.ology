b38194967feec431dcb6a2e8cf06e33591ba559b
Dollar  Luo <dollarluo@Dollars-MacBook-Air.local>
2020-01-25 20:19:14 -0500

    run child process

--- NovaHacks/server.js
+++ NovaHacks/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const childProcess = require('child_process')
 const app = express()
 const port = 3000
 
 app.get('/hello', function(req, res) {
-  res.send('test!')
+  const command = "/Users/dollarluo/Desktop/usdpython/stl_to_usdz.sh /Users/dollarluo/Desktop/3DMODELS/ironman.stl /Users/dollarluo/Desktop/3DMODELS/IRONMAN_MASK.usdz"
+  childProcess.exec(command, (error) => {
+    res.send('test!')
+  })
+  console.log('hi')
 })
 
 app.get('/', (req, res) => res.send('Hello World!'))
